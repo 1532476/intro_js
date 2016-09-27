cb1fa6ea9328bff420ef76b37a0c332c76842bf7
1532476 <1532476@utp.edu.pe>
2016-09-26 19:25:00 -0500

    Hasta el Ejercicio 08
    Introducción a JavaScript (Javier Eguíluz Pérez)
    Version online en: http://www.librosweb.es/javascript

---
+++ codigo.js
@@ -0,0 +1,4 @@
+alert("Hola Mundo");
+/*La instrucción alert() es una de las utilidades que incluye JavaScript y permite mostrar un
+mensaje en la pantalla del usuario.*/
+alert("Soy el primer Script");
